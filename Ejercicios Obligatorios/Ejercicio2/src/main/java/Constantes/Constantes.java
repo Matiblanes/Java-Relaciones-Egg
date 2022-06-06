53e66df5d1f9e613f0abdbbe499c980ea73bc5a5
Agustin <83142033+Agslz@users.noreply.github.com>
2022-06-05 21:41:33 -0300

    Update Constantes.java

--- Ejercicios Obligatorios/Ejercicio2/src/main/java/Constantes/Constantes.java
+++ Ejercicios Obligatorios/Ejercicio2/src/main/java/Constantes/Constantes.java
@@ -4,12 +4,12 @@ public class Constantes {
 
     public static final String NOMBRE = "Ingrese su nombre";
     public static final String ID = "Ingrese su ID";
-    public static final String ID_INVALIDO = "El ID ingresado no se puede utilizar, reintente: ";
+    public static final String ID_INVALIDO = "El ID ingresado no se puede utilizar, reintente";
     public static final String ID_CORRECTO = "ID ingresado correctamente";
-    public static final String JUGADOR_MOJADO = "¡El jugador se ha mojado!";
-    public static final String JUGADOR_SALVADO = "¡El jugador se ha salvado!";
-    public static final String ESPERAR_ENTER = "Presione Enter para ir a la siguiente ronda.";
-    public static final String SEGUIR_JUGANDO = "¿Quieren seguir jugando (Y/N)? ";
+    public static final String JUGADOR_MOJADO = "El jugador se ha mojado";
+    public static final String JUGADOR_SALVADO = "El jugador se ha salvado";
+    public static final String ESPERAR_ENTER = "Presione Enter para ir a la siguiente ronda";
+    public static final String SEGUIR_JUGANDO = "¿Quieren seguir jugando (Y/N)?";
     public static final String RECARGAR = "Recargando revolver para la siguiente ronda...";
 
 }
